e52031db75768ff5e6ddff0fb224e96159d0a8c2
Yatish Kumar <yak@es.net>
2024-06-22 14:57:50 -0400

    initial commit

---
+++ test_no_gui.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: test no gui
+# Author: yak
+# GNU Radio version: 3.10.9.2
+
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from test_core import test_core  # grc-generated hier_block
+
+
+
+
+class test_no_gui(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "test no gui", catch_exceptions=True)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.test_core_0 = test_core(
+            samp_rate=samp_rate,
+        )
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.test_core_0, 0), (self.blocks_null_sink_0, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.test_core_0.set_samp_rate(self.samp_rate)
+
+
+
+
+def main(top_block_cls=test_no_gui, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
