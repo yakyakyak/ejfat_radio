8f95e1e08783159f3fd9855153778eaba9198d77
Yatish Kumar <yak@es.net>
2024-06-22 15:58:55 -0400

    moved test_core block to local directory

---
+++ test_core.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Test Core
+# GNU Radio version: 3.10.9.2
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+
+
+
+
+
+
+
+class test_core(gr.hier_block2):
+    def __init__(self, samp_rate=10000):
+        gr.hier_block2.__init__(
+            self, "Test Core",
+                gr.io_signature(0, 0, 0),
+                gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.samp_rate = samp_rate
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/tmp/gnuradio_test.txt', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+
