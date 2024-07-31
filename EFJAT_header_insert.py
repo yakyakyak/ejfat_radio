409cefcdc4dc5225409905309a88694f88b9da24
Yatish Kumar <yak@es.net>
2024-07-31 16:47:38 -0400

    WIP: EJFAT_header_insert and removal testing

--- EFJAT_header_insert.py
+++ EFJAT_header_insert.py
@@ -63,17 +63,18 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 320000
+        self.samp_rate = samp_rate = 32000
+        self.payload_size = payload_size = 128
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            2, #number of inputs
+            1, #number of inputs
             None # parent
         )
         self.qtgui_time_sink_x_0_0.set_update_time(0.10)
@@ -83,7 +84,7 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0_0.enable_tags(True)
         self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0_0.enable_grid(False)
         self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
         self.qtgui_time_sink_x_0_0.enable_control_panel(False)
@@ -106,7 +107,10 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
 
         for i in range(2):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+                if (i % 2 == 0):
+                    self.qtgui_time_sink_x_0_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
             else:
                 self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
@@ -117,7 +121,7 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
             "", #name
@@ -131,7 +135,7 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0.enable_tags(True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0.enable_grid(False)
         self.qtgui_time_sink_x_0.enable_axis_labels(True)
         self.qtgui_time_sink_x_0.enable_control_panel(False)
@@ -152,9 +156,12 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(1):
+        for i in range(2):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                if (i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -165,30 +172,50 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.network_udp_source_0 = network.udp_source(gr.sizeof_float, 1, 2000, 0, 1472, False, False, False)
-        self.network_udp_sink_0 = network.udp_sink(gr.sizeof_float, 1, '127.0.0.1', 2000, 0, 1472, False)
-        self.ejfat_ejfat_sink_0 = ejfat.ejfat_sink(0)
-        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_float*1, 4)
-        self.blocks_vector_insert_x_0 = blocks.vector_insert_f([0xDE, 0xAD , 0xBE , 0xEF], 100, 0)
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_float*1, '/tmp/ejfat_packet_2.bin', False)
+        self.network_udp_source_0 = network.udp_source(gr.sizeof_char, 528, 19522, 0, 528, True, False, False)
+        self.network_udp_sink_0 = network.udp_sink(gr.sizeof_char, 1, '127.0.0.1', 19522, 0, 528, False)
+        self.ejfat_ejfat_sink_0 = ejfat.ejfat_sink(0xDEADBEEF,528)
+        self.ejfat_ejfat_pop_header_0 = ejfat.ejfat_pop_header()
+        self.blocks_vector_to_stream_0_1 = blocks.vector_to_stream(gr.sizeof_char*1, 528)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, 128)
+        self.blocks_interleave_0 = blocks.interleave(gr.sizeof_float*1, 1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_file_sink_0_1_0_0 = blocks.file_sink(gr.sizeof_char*1, '/tmp/ejfat_packet_bytes.bin', False)
+        self.blocks_file_sink_0_1_0_0.set_unbuffered(False)
+        self.blocks_file_sink_0_1_0 = blocks.file_sink(gr.sizeof_float*1, '/tmp/ejfat_packet_floats.bin', False)
+        self.blocks_file_sink_0_1_0.set_unbuffered(False)
+        self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_char*512, '/tmp/ejfat_packet_bytes3.bin', False)
+        self.blocks_file_sink_0_0_0.set_unbuffered(False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*528, '/tmp/ejfat_packet_bytes2.bin', False)
         self.blocks_file_sink_0_0.set_unbuffered(False)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, '/tmp/ejfat_packet.bin', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
+        self.blocks_deinterleave_0 = blocks.deinterleave(gr.sizeof_float*1, 1)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
+        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_CONST_WAVE, 1000, 0.9, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_vector_insert_x_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.ejfat_ejfat_sink_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0_0, 0))
-        self.connect((self.blocks_vector_insert_x_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_vector_insert_x_0, 0), (self.network_udp_sink_0, 0))
-        self.connect((self.blocks_vector_to_stream_0, 0), (self.qtgui_time_sink_x_0_0, 1))
-        self.connect((self.ejfat_ejfat_sink_0, 0), (self.blocks_vector_to_stream_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_complex_to_float_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.qtgui_time_sink_x_0_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_interleave_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_interleave_0, 1))
+        self.connect((self.blocks_deinterleave_0, 1), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_deinterleave_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_interleave_0, 0), (self.blocks_deinterleave_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.ejfat_ejfat_sink_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_file_sink_0_1_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_vector_to_stream_0_1, 0), (self.blocks_file_sink_0_1_0_0, 0))
+        self.connect((self.blocks_vector_to_stream_0_1, 0), (self.network_udp_sink_0, 0))
+        self.connect((self.ejfat_ejfat_pop_header_0, 0), (self.blocks_file_sink_0_0_0, 0))
+        self.connect((self.ejfat_ejfat_sink_0, 0), (self.blocks_vector_to_stream_0_1, 0))
         self.connect((self.network_udp_source_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.network_udp_source_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.network_udp_source_0, 0), (self.ejfat_ejfat_pop_header_0, 0))
 
 
     def closeEvent(self, event):
@@ -205,8 +232,16 @@ class EFJAT_header_insert(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
+
+    def get_payload_size(self):
+        return self.payload_size
+
+    def set_payload_size(self, payload_size):
+        self.payload_size = payload_size
 
 
 
